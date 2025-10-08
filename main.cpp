af0b66e28a5ae9dcb0d2e2bb50acb49d5925bef3
onknjpeel <k023g0103@g.neec.ac.jp>
2025-10-08 12:16:30 +0900

    エスケープキーで終了させる

--- main.cpp
+++ main.cpp
@@ -64,6 +64,10 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 		// 描画終了
 		dxCommon->PostDraw();
+
+		if (Input::GetInstance()->TriggerKey(DIK_ESCAPE)) {
+			break;
+		}
 	}
 
 	// エンジンの終了処理
