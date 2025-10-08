4516f5b5f02feb16ab9a445bddd49a28ec61d07b
onknjpeel <k023g0103@g.neec.ac.jp>
2025-10-08 16:16:18 +0900

    エスケープキーの処理をタイトルの際のみに変更

--- main.cpp
+++ main.cpp
@@ -65,8 +65,10 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		// 描画終了
 		dxCommon->PostDraw();
 
-		if (Input::GetInstance()->TriggerKey(DIK_ESCAPE)) {
-			break;
+		if (scene == kTitle) {
+			if (Input::GetInstance()->TriggerKey(DIK_ESCAPE)) {
+				break;
+			}
 		}
 	}
 
